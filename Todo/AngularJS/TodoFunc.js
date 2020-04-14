7fb3e561efdb1b4ba65196f9dfc933e7fa769feb
ido <ido29820@gmail.com>
2020-04-15 02:24:13 +0300

    Delete Function Working

--- Todo/AngularJS/TodoFunc.js
+++ Todo/AngularJS/TodoFunc.js
@@ -2,8 +2,12 @@ var TodoApp = angular.module('todoApp', []);
 
 TodoApp.controller('controller', function($scope) {
     $scope.Todos = [];
-
+    $scope.idCount = 0;
     $scope.addTodo = function() {
-        $scope.Todos.push({text: $scope.todoText, isChecked: false})
+        $scope.Todos.push({text: $scope.todoText, isChecked: false, id: $scope.idCount});
+        $scope.idCount += 1;
+    }
+    $scope.delete = function(id) {
+        $scope.Todos = $scope.Todos.filter(task => task.id != id);
     }
 });
