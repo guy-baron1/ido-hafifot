4dd23ade60a1ed81c86748dad956f9369561ccab
ido <ido29820@gmail.com>
2020-04-15 12:55:29 +0300

    Now Works With Server From VanillaJs
    Only The Add Delete And Check Will update Server Also the app is bugged on load(need to talk to you)

--- Todo/AngularJS/TodoFunc.js
+++ Todo/AngularJS/TodoFunc.js
@@ -1,19 +1,33 @@
 var TodoApp = angular.module('todoApp', []);
+let communicator = new ClientCommunicator()
 
-TodoApp.controller('controller', function($scope) {
+TodoApp.controller('controller', function($scope,$window) {
     $scope.Todos = [];
     $scope.idCount = 0;
+    
+    $window.onload = async function() {
+        data = await communicator.getTasks();
+        $scope.addTodoFromServer(data);
+    }
     $scope.addTodo = function() {
-        $scope.Todos.push({text: $scope.todoText, isChecked: false, id: $scope.idCount});
+        let newTask = {text: $scope.todoText, isChecked: false, id: $scope.idCount}
+        $scope.Todos.push(newTask);
         $scope.idCount += 1;
+        communicator.addTask(newTask)
+    }
+    $scope.addTodoFromServer = function(data) {
+        data.map(task => $scope.Todos.push(task));
+        $scope.idCount = $scope.Todos[$scope.Todos.length - 1].id + 1
     }
     $scope.delete = function(id) {
         $scope.Todos = $scope.Todos.filter(task => task.id != id);
+        communicator.removeTask(id);
     }
 
     $scope.checkTask = function(id) {
-        $scope.Todos.find(task => task.id == id).isChecked = !$scope.Todos.find(task => task.id == id).isChecked;
-        
+        let isChecked = $scope.Todos.find(task => task.id == id).isChecked;
+        $scope.Todos.find(task => task.id == id).isChecked = !isChecked;
+        communicator.checkTask(id,!isChecked)
     }
 
     $scope.deleteAllChecked = function(id) {
---
+++ Todo/AngularJS/configuration.js
@@ -0,0 +1,1 @@
+let apiUrl = "http://localhost:51875/api/todo";
